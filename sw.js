e632ffb2cfdc540fb0f8959def542f33400be113
leouch-vi <alpson2001@gmail.com>
2025-08-16 15:58:25 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,51 @@
+// NotePad Pro - Service Worker
+const CACHE_NAME = 'notepad-pro-v1.0.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Cache açıldı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache'de varsa cache'den döndür
+        if (response) {
+          return response;
+        }
+        
+        // Cache'de yoksa network'ten al
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Eski cache siliniyor:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
